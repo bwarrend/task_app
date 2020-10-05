4a59a723159ba7b4b714cce151904de7ef44d464
Brett W <bwarrend@my.hpu.edu>
2020-10-04 17:53:06 -1000

    trying to fix db

--- db/migrate/20201004213421_add_due_to_tasks.rb
+++
@@ -1,6 +0,0 @@
-class AddDueToTasks < ActiveRecord::Migration[6.0]
-  def change
-    add_column :tasks, :due_date, :datetime
-    add_column :tasks, :is_complete, :boolean
-  end
-end
--- db/migrate/20201004223026_change_duedate_in_tasks.rb
+++
@@ -1,6 +0,0 @@
-class ChangeDuedateInTasks < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :tasks, :due_date
-    add_column :tasks, :due_date, :date
-  end
-end
